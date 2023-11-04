d593ea8f433940f1784a8b48b2b2e04182d082dc
Abregoo <ale.abregotkm@gmail.com>
2023-11-03 21:38:57 -0600

    fix

---
+++ src/main/java/org/utec/apidesercionescolarqry/model/dto/AlumnoDTO.java
@@ -0,0 +1,13 @@
+package org.utec.apidesercionescolarqry.model.dto;
+
+public class AlumnoDTO {
+
+    public Integer idDependenciaEconomica;
+    public Integer idTipoResidencia;
+    public Integer idConvivenciaFamiliar;
+    public Integer idRecursoBasico;
+    public Integer idTipoVivienda;
+    public Integer idNacionalidad;
+    public Integer idSexo;
+    public Integer cantidadHermanos;
+}
